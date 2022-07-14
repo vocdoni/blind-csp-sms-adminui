d64ade13050996ca37b8e9bb6c203159dd492c2c
Òscar Casajuana <elboletaire@gmail.com>
2022-07-14 12:59:22 +0200

    Upgrade gh actions versions and add test step

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from "react"
-import { screen } from "@testing-library/react"
-import { render } from "./test-utils"
-import { App } from "./App"
+import React from 'react'
+import { screen } from '@testing-library/react'
+import { render } from './test-utils'
+import { App } from './App'
 
-test("renders learn react link", () => {
+test('renders token input', () => {
   render(<App />)
   const tokenfield = screen.getByText(/Token/i)
   expect(tokenfield).toBeInTheDocument()
