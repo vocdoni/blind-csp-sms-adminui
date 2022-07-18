de63773b74601806eea7f5e93a063c38dafaf1b8
Òscar Casajuana <elboletaire@gmail.com>
2022-07-18 17:15:43 +0200

    Fixed salt, base and explorer for release

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const API_BASE = process.env.REACT_APP_API_BASE || 'https://csp-dev.vocdoni.net/smsapi'
-export const KECCAK_SALT = process.env.REACT_APP_KECCAK_SALT || 'b297ed61bb6195919075c18677a56fd1cd7b54c4b5e64efb573e8e030fc05163'
+export const KECCAK_SALT = process.env.REACT_APP_KECCAK_SALT || '6jpC2cNVz7YrKA0MYFDIfjX9RYf5ZArQsJRdbk4jZNByzXQM8ZpCEMs8Xv7Hf7hQ'
 export const EXPLORER = process.env.REACT_APP_EXPLORER || 'https://dev.explorer.vote'
