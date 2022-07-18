e64c05c2bc58730779123993fb66c58645e7a377
Òscar Casajuana <elboletaire@gmail.com>
2022-07-18 02:54:54 +0200

    Indent fix

--- src/components/ElectionActions.tsx
+++ src/components/ElectionActions.tsx
@@ -34,53 +34,53 @@ const ElectionActions = ({election}: {election: Election}) => {
         <PopoverContent maxW='200px'>
           <PopoverArrow />
           <PopoverBody>
-          <Stack alignItems='stretch' display='flex'>
-            <UserAuthButton election={election} />
-            <If condition={election.remainingAttempts < ATTEMPTS_MAX_DEFAULT}>
-              <Then>
-                <Button
-                  size='sm'
-                  disabled={loading}
-                  isLoading={loading}
-                  onClick={() => {
-                    setLoading(true)
-                    ;(async () => {
-                      await addAttempt(election.electionId)
-                    })()
-                    setLoading(false)
-                  }}>
-                  Add an attempt
-                </Button>
-                <Button
-                  size='sm'
-                  disabled={loading}
-                  isLoading={loading}
-                  onClick={() => {
-                    setLoading(true)
-                    ;(async () => {
-                      await resetAttempts(election.electionId)
-                    })()
-                    setLoading(false)
-                  }}>
-                  Reset 5 SMS limit
-                </Button>
-              </Then>
-            </If>
-            <Button
-              size='sm'
-              disabled={loading}
-              colorScheme={useColorModeValue('yellow', 'orange')}
-              isLoading={loading}
-              onClick={() => {
-                setLoading(true)
-                ;(async () => {
-                  await setConsumed(election.electionId, !election.consumed)
-                })()
-                setLoading(false)
-              }}>
-              Set {!!election.consumed && 'NOT'} consumed
-            </Button>
-          </Stack>
+            <Stack alignItems='stretch' display='flex'>
+              <UserAuthButton election={election} />
+              <If condition={election.remainingAttempts < ATTEMPTS_MAX_DEFAULT}>
+                <Then>
+                  <Button
+                    size='sm'
+                    disabled={loading}
+                    isLoading={loading}
+                    onClick={() => {
+                      setLoading(true)
+                      ;(async () => {
+                        await addAttempt(election.electionId)
+                      })()
+                      setLoading(false)
+                    }}>
+                    Add an attempt
+                  </Button>
+                  <Button
+                    size='sm'
+                    disabled={loading}
+                    isLoading={loading}
+                    onClick={() => {
+                      setLoading(true)
+                      ;(async () => {
+                        await resetAttempts(election.electionId)
+                      })()
+                      setLoading(false)
+                    }}>
+                    Reset 5 SMS limit
+                  </Button>
+                </Then>
+              </If>
+              <Button
+                size='sm'
+                disabled={loading}
+                colorScheme={useColorModeValue('yellow', 'orange')}
+                isLoading={loading}
+                onClick={() => {
+                  setLoading(true)
+                  ;(async () => {
+                    await setConsumed(election.electionId, !election.consumed)
+                  })()
+                  setLoading(false)
+                }}>
+                Set {!!election.consumed && 'NOT'} consumed
+              </Button>
+            </Stack>
           </PopoverBody>
         </PopoverContent>
       </Portal>
